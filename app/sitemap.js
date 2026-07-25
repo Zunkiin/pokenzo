7f7f927e96d5c0e84b45c4847a50c1a9426f5bfb
Zunkiin <zunkinx@gmail.com>
2026-07-25 13:39:14 +0200

    sitemap update

--- app/sitemap.js
+++ app/sitemap.js
@@ -24,5 +24,30 @@ export default function sitemap() {
       changeFrequency: 'yearly',
       priority: 0.3,
     },
+    
+    {
+        url: 'https://www.pokenzo.com/pokemon-go/community-chat',
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 0.7,
+     },
+     {
+        url: 'https://www.pokenzo.com/pokemon-go/trades',
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 0.7,
+     },
+     {
+        url: 'https://www.pokenzo.com/pokemon-go/raids',
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 0.7,
+     },
+    {
+        url: 'https://www.pokenzo.com/pokemon-go/trainers',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.6,
+    },
   ]
 }
